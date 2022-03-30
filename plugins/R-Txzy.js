013d0004b8e830b3cc79c4a6d7550d651f504bdb
Botynyakamu <102163283+Botynyakamu@users.noreply.github.com>
2022-03-30 23:47:03 +0700

    Update R-Txzy.js

--- plugins/R-Txzy.js
+++ plugins/R-Txzy.js
@@ -1,7 +1,7 @@
 let handler = async (m, { conn }) => {
 let caption = `*TANYA KE OWNER AJA COK,GW GAK TAU!*`
 
-conn.sendButton( m.chat, caption, `©️ zifabotz`, `Otw tanya owner`, `.owner`, m)
+conn.sendButton( m.chat, caption, `©️ zifabotz`, `Otw tanya owner`, `.ownerzifa`, m)
 
        }
        
